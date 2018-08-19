42ac74134138cc500f5e2c44a98ac31fe007e72d
Juansebastian987 <juanse.bastian2012@hotmail.com>
2018-08-19 10:37:23 -0500

    Added class Ventana1TestController

---
+++ src/application/Ventana1TestController.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Ventana1TestController {
+
+	public void clickBoton(ActionEvent evento) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Ventana2Test.fxml"));
+			Scene scene = new Scene(root,400,200);
+			Stage windows = (Stage)((Node)evento.getSource()).getScene().getWindow();
+			windows.setScene(scene);
+			windows.show();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
